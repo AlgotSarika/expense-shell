1fa59d07cebb09f9c6fef5755433e821fb94c6c4
Algot Sarika <sarika@joindevops.com>
2025-01-04 08:23:14 +0530

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,18 @@
-#!/biin/bash
+#!/bin/bash
+ 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/expense-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+    if [ $? -ne 0 ]
     then
         echo -e "$2 ..$R failure $N"
         exit 1
@@ -21,28 +22,36 @@ VALIDATE(){
 
 }
 
+
 CHECK_ROOT(){
-
-
     if [ $USERID -ne 0 ]
     then 
         echo "ERROR:: you must have sudo access to execute this script"
         exit 1 #other then  0
     fi
-}
+}    
 
-echo "script strated executing at : $TIMESTAMP" &>>$LOG_FILE_NAME
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
-
 dnf install mysql-server -y &>>$LOG_FILE_NAME
-VALIDATE $? "installing mysql sever"
+VALIDATE $? "Installing MYSQL Sever"
 
 systemctl enable mysqld &>>$LOG_FILE_NAME
-VALIDATE $? "enbling mysql server"
+VALIDATE $?  "Enabling Mysql server"
 
-systemctl start mysqld &>>$LOG_FILE_NAME
+systemctl start mysqld LOG_FILE_NAME&>>$LOG_FILE_NAME
 VALIDATE $? "starting mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "setting root password"
+mysql -h mysql.online -u root -pExpenseApp@1 -e 'show databases;'
+
+if [ $? -ne 0 ]
+
+then 
+    echo "MYSQL Root password not setup" &>>$LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "sessing Root Password"
+else
+    echo -e "MYSQL Root password already setup ... $Y SKIPPING $N"
+fi
+
